e6df4681500239e4013e894321e50aead9cbb03b
Alloush <mouhamad.aloush06@gmail.com>
2025-08-14 18:19:30 +0300

    migrations

--- backend/build.sh
+++ backend/build.sh
@@ -7,7 +7,7 @@ python3 -m ensurepip --default-pip
 echo "pip install start................................................................"
 python3 -m pip install -r ../backend/requirements.txt
 echo "pip install done............"
-# python3 manage.py makemigrations --noinput
+python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput --clear
 
